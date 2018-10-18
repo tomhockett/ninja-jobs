b779e49e5ba85b2760c932e8d194b9be0f94dcfa
Tom Hockett <tom.hockett@gmail.com>
2018-10-18 17:59:20 -0500

    create user model and add password hashing

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @job = Job.new(title: "I am a title", company: "Example Company", location: "Somewhere", description: "I am a description!")
+  end
+
+  test "job should be valid" do
+    assert @job.valid?
+  end
+
+  test "title should be present" do
+    @job.title = "    "
+    assert_not @job.valid?
+  end
+
+  test "company should be present" do
+    @job.company = "    "
+    assert_not @job.valid?
+  end
+  
+  test "location should be present" do
+    @job.location = "    "
+    assert_not @job.valid?
+  end
+  
+  test "description should be present" do
+    @job.description = "    "
+    assert_not @job.valid?
+  end
 end
