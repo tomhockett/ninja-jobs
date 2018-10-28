bbeaed67e9ae80553ea197c38e0301004d76cd1f
Tom Hockett <tom.hockett@gmail.com>
2018-10-28 02:22:18 -0500

    add comment to show relation between sessions helper and our controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  # This lets all of the cool fun methods we wrote in the Sessions Helper to be used in all of our controllers! 🤓
   include SessionsHelper
 end
