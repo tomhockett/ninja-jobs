dce2d20246193e019b04fb2fba261cfc229af5a9
Tom Hockett <tom.hockett@gmail.com>
2018-11-08 13:10:05 -0600

    add remember me checkbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  root   'pages#home'
+  get    '/signup',       to: 'users#new'
+  post   '/signup',       to: 'users#create'
+  get    '/login',        to: 'sessions#new'
+  post   '/login',        to: 'sessions#create'
+  delete '/logout',       to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
 end
