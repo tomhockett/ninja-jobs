9ddafb865a7a57f97ab0ab972ec804e1cf585078
Tom Hockett <tom.hockett@gmail.com>
2018-12-19 18:53:13 -0600

    create single user page, all jobs page, and fix sidebar menu highlighting

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,11 @@ class JobsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
   end
 
   def create
